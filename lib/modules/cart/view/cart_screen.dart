c6a78e089d83cd531aaf83e34099cf04f237a97b
mohamed.gamal <mohamed.gamal@XRETAIL.com>
2023-01-31 13:15:29 +0200

    finish cart screen

--- lib/modules/cart/view/cart_screen.dart
+++ lib/modules/cart/view/cart_screen.dart
@@ -24,90 +24,95 @@ class CartScreen extends GetView<CartController> {
       onlineWidget: controller.obx(
         (data) => Column(
           children: [
-            ListView.builder(
-              controller: controller.scrollController,
-              padding: const EdgeInsets.all(10),
-              shrinkWrap: true,
-              itemCount: data!.items!.length,
-              itemBuilder: (context, index) {
-                final item = data.items?[index];
-                return ProductCardWidget(
-                  elevation: 0,
-                  isCart: true,
-                  imageHeight: .22.sw,
-                  imageWidth: .22.sw,
-                  showDashedLine: index != data.items!.length - 1,
-                  productId: item!.id!,
-                  promoText: item.promoText ?? '',
-                  hasOffer: (item.productDiscountList ?? []).isNotEmpty,
-                  offerPercentage: (item.productDiscountList ?? []).isEmpty
-                      ? ""
-                      : item.productDiscountList?.first.value ?? "",
-                  productName: item.title!,
-                  image: item.imageUrl!,
-                  brandName: item.brandName,
-                  oldPrice: item.price!,
-                  price: item.finalPrice!,
-                  count: item.qty!,
-                  maxCount: item.maxQty ?? 1,
-                  size: item.size!,
-                  color: item.color!,
-                  isHorizontal: true,
-                  isAvailable: !(item.isOutOfStock ?? false),
-                  isBogo: item.freeBogo ?? false,
-                  bogoText: item.bogoPromoText ?? '',
-                  onIncrement:
-                      data.items![index].qty == data.items![index].maxQty
-                          ? () {}
-                          : () => controller.increase(data.items![index].id!,
-                              data.items![index].qty!, context),
-                  onDecrement: () => controller.decrease(
-                      data.items![index].id!, data.items![index].qty!, context),
-                  onDelete: data.items![index].freeBogo ?? false
-                      ? null
-                      : () => controller.deleteFromCart(data.items![index].id!),
-                );
-              },
+            Expanded(
+              flex: 4,
+              child: ListView.builder(
+                controller: controller.scrollController,
+                padding: const EdgeInsets.all(10),
+                shrinkWrap: true,
+                itemCount: data!.items!.length,
+                itemBuilder: (context, index) {
+                  final item = data.items?[index];
+                  return ProductCardWidget(
+                    elevation: 0,
+                    isCart: true,
+                    imageHeight: .22.sw,
+                    imageWidth: .22.sw,
+                    showDashedLine: index != data.items!.length - 1,
+                    productId: item!.id!,
+                    promoText: item.promoText ?? '',
+                    hasOffer: (item.productDiscountList ?? []).isNotEmpty,
+                    offerPercentage: (item.productDiscountList ?? []).isEmpty
+                        ? ""
+                        : item.productDiscountList?.first.value ?? "",
+                    productName: item.title!,
+                    image: item.imageUrl!,
+                    brandName: item.brandName,
+                    oldPrice: item.price!,
+                    price: item.finalPrice!,
+                    count: item.qty!,
+                    maxCount: item.maxQty ?? 1,
+                    size: item.size!,
+                    color: item.color!,
+                    isHorizontal: true,
+                    isAvailable: !(item.isOutOfStock ?? false),
+                    isBogo: item.freeBogo ?? false,
+                    bogoText: item.bogoPromoText ?? '',
+                    onIncrement:
+                        data.items![index].qty == data.items![index].maxQty
+                            ? () {}
+                            : () => controller.increase(data.items![index].id!,
+                                data.items![index].qty!, context),
+                    onDecrement: () => controller.decrease(
+                        data.items![index].id!, data.items![index].qty!, context),
+                    onDelete: data.items![index].freeBogo ?? false
+                        ? null
+                        : () => controller.deleteFromCart(data.items![index].id!),
+                  );
+                },
+              ),
             ),
-            const Spacer(),
-            Padding(
-              padding:
-                  const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 40),
-              child: Container(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border(
-                    top: BorderSide(
-                      color: CustomThemes.appTheme.primaryColor,
-                      width: 2,
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding:
+                    const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 40),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border(
+                      top: BorderSide(
+                        color: CustomThemes.appTheme.primaryColor,
+                        width: 2,
+                      ),
                     ),
                   ),
-                ),
-                child: Obx(
-                  () => Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          CustomText(
-                              "${data.items!.length} ${Translate.items.tr}"),
-                          const SizedBox(height: 4.0),
-                          CustomText(
-                              "${Translate.total.tr} ${controller.total.value} ${Translate.egp.tr}"),
-                        ],
-                      ),
-                      const SizedBox(width: 8.0),
-                      Expanded(
-                        child: CustomBorderButton(
-                          radius: 20,
-                          color: AppColors.redColor,
-                          title: Translate.buyNow.tr,
-                          onTap: () async => controller.onTapCheckOut(context),
+                  child: Obx(
+                    () => Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CustomText(
+                                "${data.items!.length} ${Translate.items.tr}"),
+                            const SizedBox(height: 4.0),
+                            CustomText(
+                                "${Translate.total.tr} ${controller.total.value} ${Translate.egp.tr}"),
+                          ],
                         ),
-                      ),
-                    ],
+                        const SizedBox(width: 8.0),
+                        Expanded(
+                          child: CustomBorderButton(
+                            radius: 20,
+                            color: AppColors.redColor,
+                            title: Translate.buyNow.tr,
+                            onTap: () async => controller.onTapCheckOut(context),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
