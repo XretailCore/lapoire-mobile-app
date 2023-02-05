e1f13be4e660474be82ca327be52953e8fc99e20
mohamed.gamal <mohamed.gamal@XRETAIL.com>
2023-02-05 09:58:24 +0200

    enhance UI of stores_screen.dart

--- lib/modules/my_account/controllers/my_account_controller.dart
+++ lib/modules/my_account/controllers/my_account_controller.dart
@@ -47,7 +47,7 @@ class MyAccountController extends GetxController
 
   void goToProfile() {
     if (userSharedPrefrenceController.isUser) {
-      Get.toNamed(Routes.branchs);
+      Get.toNamed(Routes.profile);
     } else {
       Get.toNamed(Routes.sign);
     }
