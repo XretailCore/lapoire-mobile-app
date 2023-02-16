9039f891c03cda444fa1057befa4732f7d2251cd
mohamed.gamal <mohamed.gamal@XRETAIL.com>
2023-02-16 14:04:02 +0200

    Fix BUG-4979

--- lib/modules/order_details/widgets/order_details_items_widget.dart
+++ lib/modules/order_details/widgets/order_details_items_widget.dart
@@ -1,3 +1,5 @@
+import 'package:dotted_line/dotted_line.dart';
+import 'package:imtnan/core/utils/app_colors.dart';
 import 'package:linktsp_api/linktsp_api.dart';
 
 import '../../../core/components/custm_product_card.dart';
@@ -19,28 +21,33 @@ class OrderDetailsItemsWidget extends GetView<OrderDetailsController> {
       separatorBuilder: (context, index) => const SizedBox(height: 10),
       itemBuilder: (context, index) {
         final item = orderDetailsModel.orderItems!.elementAt(index);
-        return ProductCardWidget(
-          elevation: 0,
-          isHorizontal: true,
-          productId: item.skuId!,
-          promoText: item.promoText ?? '',
-          productName: item.title ?? "",
-          image: item.imageUrl ?? "",
-          imageHeight: .35.sw,
-          imageWidth: .35.sw,
-          oldPrice: item.price,
-          price: item.finalPrice ?? 0,
-          isBogo: !(item.bogoPromoText == null),
-          hasOffer: !(item.productDiscountList == null ||
-              item.productDiscountList!.isEmpty),
-          offerPercentage: item.productDiscountList == null ||
-                  item.productDiscountList!.isEmpty
-              ? ""
-              : item.productDiscountList?.first.value ?? "",
-          size: item.size,
-          count: item.qty,
-          isAvailable: !(item.isOutOfStock ?? false),
-          bogoText: item.bogoPromoText ?? '',
+        return Column(
+          children: [
+            ProductCardWidget(
+              elevation: 0,
+              isHorizontal: true,
+              productId: item.skuId!,
+              promoText: item.promoText ?? '',
+              productName: item.title ?? "",
+              image: item.imageUrl ?? "",
+              imageHeight: .35.sw,
+              imageWidth: .35.sw,
+              oldPrice: item.price,
+              price: item.finalPrice ?? 0,
+              isBogo: !(item.bogoPromoText == null),
+              hasOffer: !(item.productDiscountList == null ||
+                  item.productDiscountList!.isEmpty),
+              offerPercentage: item.productDiscountList == null ||
+                      item.productDiscountList!.isEmpty
+                  ? ""
+                  : item.productDiscountList?.first.value ?? "",
+              size: item.size,
+              count: item.qty,
+              isAvailable: !(item.isOutOfStock ?? false),
+              bogoText: item.bogoPromoText ?? '',
+            ),
+            index!=orderDetailsModel.orderItems!.length-1?const DottedLine(dashColor: AppColors.redColor,) :const SizedBox()
+          ],
         );
       },
     );
